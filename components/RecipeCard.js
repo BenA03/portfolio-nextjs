39bc24c0294bc007ffdbd2ed5ccfa9e319aae4b1
BenA03 <bma54@pitt.edu>
2023-04-16 09:40:23 -0700

    sample attempt with dynamically created content

--- components/RecipeCard.js
+++ components/RecipeCard.js
@@ -3,7 +3,7 @@ function RecipeCard(props)
     return (
         <div class="card small-card center vertical-spacing">
             <div class="card-body">
-                This is some text within a card body.
+                {props.content}
             </div>
         </div>);
 }
--- components/Recipes.js
+++ components/Recipes.js
@@ -1,12 +1,16 @@
 import RecipeCard from "@/components//RecipeCard";
 
+let items = ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5'];
+
+let itemList = items.map((item, index) => {
+    return <RecipeCard key={index} content={item}/>
+})
+
 function Recipes() {
     return (
-        <>
-            <RecipeCard />
-            <RecipeCard />
-            <RecipeCard />
-        </>
+        <ul>
+            {itemList}
+        </ul>
     );
 }
 
