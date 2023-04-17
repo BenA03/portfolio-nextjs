066774d2055251c75a0c7d4efd3cd60df12c4fc1
BenA03 <bma54@pitt.edu>
2023-04-17 10:35:55 -0700

    user can remove cards as well

--- components/Recipes.js
+++ components/Recipes.js
@@ -3,27 +3,25 @@ import { useState } from "react";
 
 function instantiateItems() {
     let recipeArr = [];
-    for (let i = 0; i < 5; i++) {
-        recipeArr.push("Test");
+    for (let i = 0; i < 3; i++) {
+        recipeArr.push("This is a card that the user did not make");
     }
     return recipeArr;
-}
-
-function alterList(list) {
-    console.log("Hi!");
-    list.push("Hi!");
 }
 
 function Recipes() {
     const [list, alterList] = useState(instantiateItems());
     let recipeList = list.map((item, index) => {
-        return <RecipeCard key={index} content={item} />
+        return <RecipeCard key={index} content={item + " (card number: " + (index + 1) + ")"} />
     })
     return (
         <>
             {recipeList}
             <div className="centerFlex">
-                    <button className="btn btn-primary center btn-lg vertical-spacing" onClick={() => alterList([...list, "Hi!"])}>+ Add a card</button>
+                <div className="btn-group center" role="group">
+                    <button type="button" className="btn btn-success vertical-spacing" onClick={() => alterList([...list, "This is a card the user added"])}>+ Add a card</button>
+                    <button type="button" className="btn btn-danger vertical-spacing" onClick={() => alterList(list.slice(0, list.length - 1))}>Remove a card -</button>
+                </div>
             </div>
         </>
     );
